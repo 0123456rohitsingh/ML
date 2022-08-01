dffb5c4be22f1277ccfb047de6387a9e4835d36a
Rohit Singh <rohitsingh@Rohits-MacBook-Air.local>
2022-08-02 03:15:53 +0530

    Checking Automatic Trigger for Deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Success"
+    return "Successfull Development of CI/CD Pipeline"
 
 if __name__=="main":
     app.run(debug=True)
